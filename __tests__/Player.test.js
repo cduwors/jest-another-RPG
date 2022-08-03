5cc73feb29b45c19c958ed1683147deaee24c9b8
cduwors <christinaduwors@gmail.com>
2022-08-03 13:25:32 -0400

    player prototype set up

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,11 +1,9 @@
 const Potion = require("../lib/Potion");
+const Player = require("../lib/Player");
 
 jest.mock("../lib/Potion");
 
 console.log(new Potion());
-
-const Player = require("../lib/Player");
-const { JestHook } = require("jest-watcher");
 
 test("creates player object", () => {
 	const player = new Player("Dave");
@@ -18,4 +16,23 @@ test("creates player object", () => {
 	expect(player.inventory).toEqual(
 		expect.arrayContaining([expect.any(Object)])
 	);
+});
+
+test("gets player's stats as an object", () => {
+	const player = new Player("Dave");
+
+	expect(player.getStats()).toHaveProperty("potions");
+	expect(player.getStats()).toHaveProperty("health");
+	expect(player.getStats()).toHaveProperty("strength");
+	expect(player.getStats()).toHaveProperty("agility");
+});
+
+test("gets inventory from player or returns false", () => {
+	const player = new Player("Dave");
+
+	expect(player.getInventory()).toEqual(expect.any(Array));
+
+	player.inventory = [];
+
+	expect(player.getInventory()).toEqual(false);
 });
